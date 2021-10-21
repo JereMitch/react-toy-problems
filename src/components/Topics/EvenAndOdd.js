8796db60847971e27324585e58ae6e97c2bc3817
JereMitch <jere.mitch8@gmail.com>
2021-10-21 14:14:34 -0600

    finished onclick

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -15,32 +15,34 @@ export default class EvenAndOdd extends Component {
         const onChange = (e) => {
             const handleChange = (input) => {
                 this.setState({ userInput: input})
-                console.log(input)
+                // console.log(input)
             }
             handleChange(e.target.value)
         }
 
-        const onclick = (userInput) => {
-            let array = userInput
+        const onClick = () => {
+          const handleClick = (userInput) => {
+            let array = userInput.split(' ')
             let even = []
             let odd = []
 
-            for(let i = 0; i < array.length; i++){
+            for(let i = 0; i < array.length; i++)
                 if(array[i] % 2 === 0){
                     even.push(array[i])
                 } else {
                     odd.push(array[i])
                 }
-            }
-            this.setState({ evenArray: even, oddArray: odd })
+                // console.log(even, odd)
+                this.setState({ evenArray: even, oddArray: odd })
+          }
+            handleClick(this.state.userInput)
         }
 
     return (
       <div className="puzzleBox evenAndOddPB">
         <h4>Evens and Odds</h4>
-        <input type="number"  className="inputLine" onChange = {onChange} />
-          
-        <button className="confirmationButton" onclick={onclick}>Click Me!</button>
+        <input className="inputLine" onChange = {onChange} />
+        <button className="confirmationButton" onClick = {onClick}>Click Me!</button>
         <span className="resultsBox">Evens: {this.state.evenArray}</span>
         <span className="resultsBox">Odds: {this.state.oddArray}</span>
       </div>
